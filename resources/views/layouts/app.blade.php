e38570cbb9ed84b72dbc1aac712708c00a290298
Fathiyyah Muthi <83500997+muthiasyiffa@users.noreply.github.com>
2023-05-23 19:56:34 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,10 @@
 
     <!-- jQuery terlebih dahulu -->
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-
+    
+    <script src="https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    
     <!-- Bootstrap JavaScript -->
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"></script>
 
     <script src="https://unpkg.com/@popperjs/core@2"></script>
