a3324789a1722b9b6a54ab9f320d6af5306e3632
Fathiyyah Muthi <fathiyyahasyifa123@gmail.com>
2023-05-26 21:16:05 +0700

    adj v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-    <title>MISSO XL</title>
+    <title>MISSO</title>
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
   </head>
